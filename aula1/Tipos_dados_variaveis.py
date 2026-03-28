487350026539cd70e4ec9c1d4137c06d2a8d8824
MuriloS_estudant <ms6247948@gmail.com>
2026-03-28 17:55:08 -0300

    Add files via upload

--- aula1/Tipos_dados_variaveis.py
+++ aula1/Tipos_dados_variaveis.py
@@ -114,3 +114,42 @@ print(x ** y)
 
 print(x // y)
 print(x % y)
+print() # imprime algo ambiente de teste 
+
+
+var =  10 # variáveis 
+
+
+# nome = input('Digite seu nome:  ') # dinamico 
+# # 
+
+
+# print(nome)
+
+
+# nome  = 'Julia'
+
+
+# nome  =  input('Nome: ')
+
+
+
+print('CADASTRE SEUS DADOS: ')
+# int()  float() str() bool() type casting
+
+
+nome = input('Nome: ')
+idade  = int(input('Idade: ')) + 10
+endereco  = input('Endereço: ')
+curso = input('Curso: ')
+altura = float(input('Altura'))
+saldo_banco = float(input('Saldo: '))
+
+
+print('NOME:', nome)
+print('IDADE:', idade)
+print('ENDEREÇO:', endereco)
+print('CURSO:', curso)
+print('ALTURA:', altura)
+print('SALDO NO BANCO:', saldo_banco)
+
---
+++ aula1/atividade_1.py
@@ -0,0 +1,39 @@
+#Crie um programa que:
+#Peça ao usuário dois números. 
+
+n1 = float(input('numero: '))
+n2 = float(input('numero: '))
+
+# use apenas:
+# variáveis 
+# print()
+# input()
+# sinais aritmético + - ? * / ** % 
+# sinais lógicos <  > >=  <=  != == 
+
+# mostre:
+# 
+# A soma, subtração, multiplicação e divisão.
+
+soma =  n1 + n2
+print(soma)
+subtracao = n1  - n2
+print(subtracao)
+mult =  n1 + n2
+print(mutl)
+div = n1 /n2 
+print(div)
+
+
+# Verifique: 
+
+# Se os números são iguais.
+
+# Se o primeiro número é maior que o segundo.
+
+#Se pelo menos um dos números é maior que 10
+
+
+
+
+
