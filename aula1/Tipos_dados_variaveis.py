1bb72207b617cc4844de60d9d36300d761db0c42
MuriloS_estudant <ms6247948@gmail.com>
2026-03-28 12:49:01 -0300

    Arquivo Aula 1
    tipos de dados primitivos em Python

---
+++ aula1/Tipos_dados_variaveis.py
@@ -0,0 +1,116 @@
+print('Hello word')
+
+# indentação -  organizar o código
+# indentação -  organizar o código
+
+
+print()
+
+
+
+# variáveis
+
+
+imc = 0
+
+
+# Não podem por numero
+# caracteres especiais # @% &*
+
+
+# pode começar por letra
+# _ underline pode utilizar
+
+
+# seguida de um igual 
+
+
+# ESTRUTURA DE DADO
+
+
+imc = 0 
+altura  =  1.70
+peso = 62.0
+imc =  peso // (altura ** 2)
+print(imc)
+
+
+# sinais aritméticos 
+# +   -    *   /   %   **  // 
+
+
+print('SINAIS ARITMÉTICOS')
+# saida sem é um número 
+
+
+x = 10
+y = 20
+x = 200
+
+
+print(x + y)
+print(x - y)
+print(x / y)
+print(x * y)
+print(x ** y)
+
+
+
+print(x // y)
+print(x % y)
+
+
+print()
+
+
+
+# variáveis
+
+
+imc = 0
+
+
+# Não podem por numero
+# caracteres especiais # @% &*
+
+
+# pode começar por letra
+# _ underline pode utilizar
+
+
+# seguida de um igual 
+
+
+# ESTRUTURA DE DADO
+
+
+imc = 0 
+altura  =  1.70
+peso = 62.0
+imc =  peso // (altura ** 2)
+print(imc)
+
+
+# sinais aritméticos 
+# +   -    *   /   %   **  // 
+
+
+print('SINAIS ARITMÉTICOS')
+# saida sem é um número 
+
+
+x = 10
+y = 20
+x = 200
+
+
+print(x + y)
+print(x - y)
+print(x / y)
+print(x * y)
+print(x ** y)
+
+
+
+print(x // y)
+print(x % y)
